66fe1ac9fa86d9d7aaae276718bf951cbdacf8ae
seung-ook <67666332+seung-ook@users.noreply.github.com>
2020-07-07 17:26:59 +0900

    Initial Commit

---
+++ swift_study/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_study
+//
+//  Created by HanaTI on 2020/07/07.
+//  Copyright © 2020 HanaTI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
